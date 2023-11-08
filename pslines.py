5d6d5fa89da25fb2acc19158c6ea6bc3d84cce06
satyapan <satyapan.iiserm@gmail.com>
2023-11-08 17:53:45 +0100

    Change Start/End UTC to Start UTC/Duration

--- pslines.py
+++ pslines.py
@@ -63,51 +63,51 @@ class get_horizon:
         self.dec0 = dec0*np.pi/180
         self.date = None
         self.utc = None
+        self.duration = None
         self.factor_cosmo = ((cosmo.comoving_transverse_distance(self.z)/((const.c*(1+self.z)**2)/(cosmo.H(self.z)*1.4204057e9*u.Hz))).to(u.Hz).value)/freq_cen
     
     def get_factor_lst(self, lst):
         factor = (1+np.sqrt(1-(np.sin(self.lat)*np.sin(self.dec0)+np.cos(self.lat)*np.cos(self.dec0)*np.cos(lst-self.ra0))**2))/(np.sin(self.lat)*np.sin(self.dec0)+np.cos(self.lat)*np.cos(self.dec0)*np.cos(lst-self.ra0))
         if factor < 0:
-            raise ValueError('Phase Center goes below the horizon!')
-        else:
-            return factor
+            print('Phase Center goes below the horizon at lst = %sh!'%(np.round(lst,3)))
+        return factor
         
     def get_factor_max(self, N=100):
-        utc_start = time_to_h(self.utc[0])
-        utc_end = time_to_h(self.utc[1])
-        if isinstance(self.date, tuple):
-            if len(self.date) > 2 or (dec_to_str(self.utc[1]) > dec_to_str(self.utc[0])):
-                raise ValueError('If observation spans more than 24 hours, use full synthesis line.')
-            else:
-                utc_range = np.linspace(utc_start, 24+utc_end, N)
-                factors = []
-                for i in range(N):
-                    if utc_range[i] < 24:
-                        lst = utc_to_lst(utc_range[i], self.date[0], self.lat, self.lon)
-                    else:
-                        lst = utc_to_lst(utc_range[i]-24, self.date[1], self.lat, self.lon)
-                    factors.append(self.get_factor_lst(lst))
-                return np.amax(factors)
+        date_start = self.date
+        utc_start = time_to_h(self.utc)
+        date_end = self.date
+        utc_end = time_to_h(self.utc) + self.duration
+        if utc_end >= 24:
+            utc_end -= 24
+            date_end = (datetime.datetime.strptime(date_start, '%Y-%m-%d')+datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        if utc_end > utc_start:
+            utc_range = np.linspace(utc_start, utc_end, N)
+            factors = []
+            for i in utc_range:
+                lst = utc_to_lst(i, date_start, self.lat, self.lon)
+                factors.append(self.get_factor_lst(lst))
+            return np.amax(factors)
         else:
-            if (time_to_h(self.utc[1]) <= time_to_h(self.utc[0])):
-                raise ValueError('End UTC must be greater than start UTC for an observation spanning a single day.')
-            else:
-                utc_range = np.linspace(utc_start, utc_end, N)
-                factors = []
-                for i in utc_range:
-                    lst = utc_to_lst(i, self.date, self.lat, self.lon)
-                    factors.append(self.get_factor_lst(lst))
-                return np.amax(factors)
+            utc_range = np.linspace(utc_start, 24+utc_end, N)
+            factors = []
+            for i in range(N):
+                if utc_range[i] < 24:
+                    lst = utc_to_lst(utc_range[i], date_start, self.lat, self.lon)
+                else:
+                    lst = utc_to_lst(utc_range[i]-24, date_end, self.lat, self.lon)
+                factors.append(self.get_factor_lst(lst))
+            return np.amax(factors)
     
     def get_horizon_lst(self, kper_list):
-        if isinstance(self.utc, tuple):
-            if len(self.utc) != 2:
-                raise ValueError('UTC should be either a single value or a tuple of two values corresponding to the start and end of the observation.')
-            else:
-                factor = self.get_factor_max()
-        else:
+        if self.duration == None or self.duration == 0:
             lst = utc_to_lst(self.utc, self.date, self.lat, self.lon)
             factor = self.get_factor_lst(lst)
+        elif self.duration > 24:
+            raise ValueError('If observation spans more than 24 hours, use full synthesis line.')
+        elif self.duration < 0:
+            raise ValueError('Duration needs to be positive.')
+        else:
+            factor = self.get_factor_max()
         return factor*self.factor_cosmo*kper_list
     
     def get_horizon_full(self, kper_list):    
@@ -117,7 +117,7 @@ class get_horizon:
     def get_horizon_flat(self, kper_list):
         return self.factor_cosmo*np.array(kper_list)
     
-    def plot(self, kper_list, kind='lst', utc=None, date=None, ax=None, **kargs):
+    def plot(self, kper_list, kind='lst', utc=None, date=None, duration=None, ax=None, **kargs):
         """
         Plot horizon line.
 
@@ -128,17 +128,17 @@ class get_horizon:
             'full': Plot horizon line assuming full synthesis of 24 hours. 
             'flat': Plot horizon line with flat sky approximation.
         ax (optional, None): Axis on which to plot horizon line. If no axis is given, a new figure is created and the horizon line is plotted on it.
-        date (string or 2-tuple): Necessary only when using kind='lst'. Date of observation (string in the format 'yyyy-mm-dd'). 
-            If observation spans two days, use a 2-tuple: (day1, day2) with day1 and day2 in the format 'yyyy-mm-dd'. It is then necessary to use a 2-tuple for utc: (start_utc, end_utc).
-        utc (optional, float or string or 2-tuple): Necessary only when using kind='lst'. There are 3 options:
-            If 2-tuple (start_utc, end_utc) is given, horizon line for that observation duration is plotted when using kind = 'lst'. start_utc and end_utc can be either strings in the format 'hh:mm:ss' or floats in decimal hours.
-            If single value is given (string in the format 'hh:mm:ss' or float in decimal hours), horizon line at that utc is plotted when using kind = 'lst'. 
-            If blank, only full synthesis line and flat sky line can be plotted.
+        date (string): Necessary only when using kind='lst'. Start date of observation (string in the format 'yyyy-mm-dd').
+        utc (optional, float or string): Necessary only when using kind='lst'. Start UTC of the observation (string in the format 'hh:mm:ss' or float in decimal hours).
+        duration (optional, float): Duration of the observation in hours. 
+            If duration = 0 or None, horizon line at the given utc is plotted.
+            Otherwise horizon line for the observation duration is plotted.
         """
         if kind=='lst' and (utc==None or date==None):
             raise ValueError("Argument utc and date are necessary if using kind='lst'")
         self.utc = utc
         self.date = date
+        self.duration = duration
         if ax is None:
             fig,ax = plt.subplots()    
         if kind == 'lst':
@@ -163,7 +163,7 @@ class get_horizon:
         else:
             print('Full synthesis line is vertical!')
             
-    def get_line(self, kper_list, kind='lst', utc=None, date=None):
+    def get_line(self, kper_list, kind='lst', utc=None, date=None, duration=None):
         """
         Return k_parallel values for the horizon line.
         
@@ -174,6 +174,7 @@ class get_horizon:
             raise ValueError("Argument utc and date are necessary if using kind='lst'")
         self.utc = utc
         self.date = date
+        self.duration = duration
         if kind == 'lst':
             kpar_list = self.get_horizon_lst(kper_list)
         elif kind == 'full':
@@ -211,43 +212,40 @@ class get_source:
         self.dec = dec*np.pi/180
         self.date = None
         self.utc = None
+        self.duration = None
         self.factor_cosmo = ((cosmo.comoving_transverse_distance(self.z)/((const.c*(1+self.z)**2)/(cosmo.H(self.z)*1.4204057e9*u.Hz))).to(u.Hz).value)/freq_cen
     
     def get_factor_lst(self, lst):
         return ((np.sin(lst-self.ra)*np.cos(self.dec) - np.sin(lst-self.ra0)*np.cos(self.dec0))**2 + (np.sin(self.lat)*np.cos(self.dec)*np.cos(lst-self.ra) - np.sin(self.lat)*np.cos(self.dec0)*np.cos(lst-self.ra0) - np.sin(self.dec)*np.cos(self.lat) + np.sin(self.dec0)*np.cos(self.lat))**2)/np.sqrt((np.sin(lst-self.ra)*np.cos(self.dec) - np.sin(lst-self.ra0)*np.cos(self.dec0))**2 - ((np.sin(lst-self.ra)*np.cos(self.dec) - np.sin(lst-self.ra0)*np.cos(self.dec0))*np.sin(lst-self.ra0)*np.cos(self.dec0) + (np.sin(self.lat)*np.cos(self.dec0)*np.cos(lst-self.ra0) - np.sin(self.dec0)*np.cos(self.lat))*(np.sin(self.lat)*np.cos(self.dec)*np.cos(lst-self.ra) - np.sin(self.lat)*np.cos(self.dec0)*np.cos(lst-self.ra0) - np.sin(self.dec)*np.cos(self.lat) + np.sin(self.dec0)*np.cos(self.lat)))**2 + (np.sin(self.lat)*np.cos(self.dec)*np.cos(lst-self.ra) - np.sin(self.lat)*np.cos(self.dec0)*np.cos(lst-self.ra0) - np.sin(self.dec)*np.cos(self.lat) + np.sin(self.dec0)*np.cos(self.lat))**2)
     
     def get_factor_range(self, N=100):
-        utc_start = time_to_h(self.utc[0])
-        utc_end = time_to_h(self.utc[1])
-        if isinstance(self.date, tuple):
-            if len(self.date) > 2 or (dec_to_str(self.utc[1]) > dec_to_str(self.utc[0])):
-                raise ValueError('Observation should be less than 24 hours.')
-            else:
-                utc_range = np.linspace(utc_start, 24+utc_end, N)
-                factors = []
-                for i in range(N):
-                    if utc_range[i] < 24:
-                        lst = utc_to_lst(utc_range[i], self.date[0], self.lat, self.lon)
-                    else:
-                        lst = utc_to_lst(utc_range[i]-24, self.date[1], self.lat, self.lon)
-                    factors.append(self.get_factor_lst(lst))
-                return np.amin(factors),np.amax(factors)
+        date_start = self.date
+        utc_start = time_to_h(self.utc)
+        date_end = self.date
+        utc_end = time_to_h(self.utc) + self.duration
+        if utc_end >= 24:
+            utc_end -= 24
+            date_end = (datetime.datetime.strptime(date_start, '%Y-%m-%d')+datetime.timedelta(days=1)).strftime('%Y-%m-%d')        
+        if utc_end > utc_start:
+            utc_range = np.linspace(utc_start, utc_end, N)
+            factors = []
+            for i in utc_range:
+                lst = utc_to_lst(i, date_start, self.lat, self.lon)
+                factors.append(self.get_factor_lst(lst))
+            return np.amin(factors), np.amax(factors)
         else:
-            if (time_to_h(self.utc[1]) <= time_to_h(self.utc[0])):
-                raise ValueError('End UTC must be greater than start UTC for an observation spanning a single day.')
-            else:
-                utc_range = np.linspace(utc_start, utc_end, N)
-                factors = []
-                for i in utc_range:
-                    lst = utc_to_lst(i, self.date, self.lat, self.lon)
-                    factors.append(self.get_factor_lst(lst))
-                return np.amin(factors), np.amax(factors)
+            utc_range = np.linspace(utc_start, 24+utc_end, N)
+            factors = []
+            for i in range(N):
+                if utc_range[i] < 24:
+                    lst = utc_to_lst(utc_range[i], date_start, self.lat, self.lon)
+                else:
+                    lst = utc_to_lst(utc_range[i]-24, date_end, self.lat, self.lon)
+                factors.append(self.get_factor_lst(lst))
+            return np.amin(factors),np.amax(factors)
     
     def get_source_range(self, kper_list):
-        if len(self.utc) != 2:
-            raise ValueError('UTC should be either a single value or a tuple of two values corresponding to the start and end of the observation')
-        else:
-            factor_min, factor_max = self.get_factor_range()
+        factor_min, factor_max = self.get_factor_range()
         return factor_min*self.factor_cosmo*kper_list, factor_max*self.factor_cosmo*kper_list
     
     def get_source_lst(self, kper_list):    
@@ -259,7 +257,7 @@ class get_source:
         angle = angular_distance(self.ra,self.dec,self.ra0,self.dec0)
         return self.factor_cosmo*np.array(kper_list)*np.sin(angle)
         
-    def plot(self, kper_list, kind='lst', ax=None, utc=None, date=None, **kargs):
+    def plot(self, kper_list, kind='lst', ax=None, utc=None, date=None, duration=None, **kargs):
         """
         Plot source line.
 
@@ -269,33 +267,35 @@ class get_source:
             'lst': Plot source lines at a particular lst or for a given lst range. 
             'flat': Plot horizon line with flat sky approximation.
         ax (optional, None): Axis on which to plot source line. If no axis is given, a new figure is created and the source line is plotted on it.
-        date (string or 2-tuple): Necessary only when using kind='lst'. Date of observation (string in the format 'yyyy-mm-dd'). 
-            If observation spans two days, use a 2-tuple: (day1, day2) with day1 and day2 in the format 'yyyy-mm-dd'. It is then necessary to use a 2-tuple for utc: (start_utc, end_utc).
-        utc (optional, float or string or 2-tuple): Necessary only when using kind='lst'. 3 options:
-            If 2-tuple (start_utc, end_utc) is given, two source lines (minimum and maximum) for that observation duration is plotted when using kind = 'lst'. start_utc and end_utc can be either strings in the format 'hh:mm:ss' or floats in decimal hours.
-            If single value is given (string in the format 'hh:mm:ss' or float in decimal hours), source line at that utc is plotted when using kind = 'lst'.
-            If blank, only flat sky approximation line can be plotted        
+        date (string): Necessary only when using kind='lst'. Start date of observation (string in the format 'yyyy-mm-dd').
+        utc (optional, float or string): Necessary only when using kind='lst'. Start UTC of the observation (string in the format 'hh:mm:ss' or float in decimal hours).
+        duration (optional, float): Duration of the observation in hours. 
+            If duration = 0 or None, source line at the given utc is plotted.
+            Otherwise two source lines (minimum and maximum) for that observation duration is plotted.      
         """
         if kind=='lst' and (utc==None or date==None):
             raise ValueError("Argument utc and date are necessary if using kind='lst'")
         self.utc = utc
         self.date = date
+        self.duration = duration
         if ax is None:
             fig,ax = plt.subplots()    
         if kind == 'lst':
-            if isinstance(self.utc, tuple):
+            if duration == None or duration == 0:
+                kpar_list = self.get_source_lst(kper_list)
+                kargs.setdefault('color', 'black')
+                kargs.setdefault('linestyle', 'solid')
+                kargs.setdefault('linewidth', 2)
+                ax.plot(kper_list, kpar_list, **kargs)   
+            elif duration < 0 or duration > 24:
+                raise ValueError('Duration needs to be between 0 and 24.')
+            else:
                 kpar_list_min, kpar_list_max = self.get_source_range(kper_list)
                 kargs.setdefault('color', 'black')
                 kargs.setdefault('linestyle', 'solid')
                 kargs.setdefault('linewidth', 2)
                 ax.plot(kper_list, kpar_list_min, **kargs)
                 ax.plot(kper_list, kpar_list_max, **kargs)
-            else:
-                kpar_list = self.get_source_lst(kper_list)
-                kargs.setdefault('color', 'black')
-                kargs.setdefault('linestyle', 'solid')
-                kargs.setdefault('linewidth', 2)
-                ax.plot(kper_list, kpar_list, **kargs)             
         elif kind == 'flat':
             kpar_list = self.get_source_flat(kper_list)
             kargs.setdefault('color', 'black')
@@ -305,7 +305,7 @@ class get_source:
         else:
             raise ValueError('kind = "lst"/flat"')
             
-    def get_line(self, kper_list, kind='lst', utc=None, date=None):
+    def get_line(self, kper_list, kind='lst', utc=None, date=None, duration=None):
         """
         Return k_parallel values for the source line.
         
@@ -316,13 +316,16 @@ class get_source:
             raise ValueError("Argument utc and date are necessary if using kind='lst'")
         self.utc = utc
         self.date = date
+        self.duration = duration
         if kind == 'lst':
-            if isinstance(self.utc, tuple):
-                kpar_list_min, kpar_list_max = self.get_source_range(kper_list)
-                return kpar_list_min, kpar_list_max
-            else:
+            if duration == None or duration == 0:
                 kpar_list = self.get_source_lst(kper_list)
                 return kpar_list
+            elif duration < 0 or duration > 24:
+                raise ValueError('Duration needs to be between 0 and 24.')
+            else:
+                kpar_list_min, kpar_list_max = self.get_source_range(kper_list)
+                return kpar_list_min, kpar_list_max
         elif kind == 'flat':
             kpar_list = self.get_source_flat(kper_list)
             return kpar_list
